62d6ab58cbff8e11dc3c9692d9c99589136bcf0e
brunokiryluk <51805262+brunokiryluk@users.noreply.github.com>
2023-12-07 02:12:20 -0300

    optimize imports and doing first inditex integration test

--- src/test/java/com/example/inditexecommerce/InditexEcommerceApplicationIntegrationTests.java
+++ src/test/java/com/example/inditexecommerce/InditexEcommerceApplicationIntegrationTests.java
@@ -1,27 +1,15 @@
 package com.example.inditexecommerce;
 
-import com.example.inditexecommerce.infraestructure.controller.dtos.response.PriceDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static java.lang.reflect.Array.get;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,25 +21,39 @@ class InditexEcommerceApplicationIntegrationTests {
     private MockMvc mvc;
 
     @Test
-    public void test() throws Exception {
-//        ObjectMapper mapper = new ObjectMapper();
-//        String brandId = "1";
-//        String productId = "35455";
-//        MvcResult result = mvc.perform(MockMvcRequestBuilders
-//                        .get("/prices/brands/{brandId}/products/{productId}?dateRequest=2020-06-14-13.00.00", brandId, productId)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                //.andExpect((jsonPath("[0].finalPrice", is("25.45")));
-//                //.andExpect(MockMvcResultMatchers.jsonPath("$.employees[*].employeeId").isNotEmpty());
-//        String responseAsString = result.getResponse().getContentAsString();
-//        PriceDTO priceDTO = mapper.reader().forType(PriceDTO.class).readValue(responseAsString);
-//        assertNotNull(priceDTO);
-        //TODO: Hacer assertions de campos del response
+    public void test1InditextChallenge() throws Exception {
+        /**
+         * Test 1 inditex:
+         * "Test 1: petición a las 10:00 del día 14 del producto 35455   para la brand 1 (ZARA)"
+         * En este test se comprueba que la aplicacion devuelve un PriceDTO cuyo precio final es 35.50
+         */
+        String brandId = "1";
+        String productId = "35455";
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/prices/brands/{brandId}/products/{productId}?dateRequest=2020-06-14-10.00.00", brandId, productId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.priceId").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.priceId", is(1)))
 
-        //assertNotNull(result);
-        //.andExpect(MockMvcResultMatchers.jsonPath("$.employees").exists())
-    //.andExpect(MockMvcResultMatchers.jsonPath("$.employees[*].employeeId").isNotEmpty());
-}
+                .andExpect(MockMvcResultMatchers.jsonPath("$.brand").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.brand.name", is("ZARA")))
+
+                .andExpect(MockMvcResultMatchers.jsonPath("$.applicablePriceListId").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.applicablePriceListId", is("1")))
+
+                .andExpect(MockMvcResultMatchers.jsonPath("$.startDate").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.startDate", is("2020-06-14 00:00:00.0")))
+
+                .andExpect(MockMvcResultMatchers.jsonPath("$.endDate").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.endDate", is("2020-12-31 23:59:59.0")))
+
+                .andExpect(MockMvcResultMatchers.jsonPath("$.finalPrice").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.finalPrice", is("35.5")))
+
+                .andExpect(MockMvcResultMatchers.jsonPath("$.currency").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.currency", is("EUR")));
+    }
 
 }

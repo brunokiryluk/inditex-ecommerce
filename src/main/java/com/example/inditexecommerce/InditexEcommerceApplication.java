25603c1e61feaac6053e450395a19a9872180517
brunokiryluk <51805262+brunokiryluk@users.noreply.github.com>
2023-12-07 02:11:37 -0300

    optimize imports

--- src/main/java/com/example/inditexecommerce/InditexEcommerceApplication.java
+++ src/main/java/com/example/inditexecommerce/InditexEcommerceApplication.java
@@ -7,9 +7,6 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class InditexEcommerceApplication {

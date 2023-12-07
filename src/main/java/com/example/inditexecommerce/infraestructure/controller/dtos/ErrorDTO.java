5911e48792367d9bbb30ed5b30c9cf570d3717f9
brunokiryluk <51805262+brunokiryluk@users.noreply.github.com>
2023-12-07 02:10:17 -0300

    Remove unnused DTOs

--- src/main/java/com/example/inditexecommerce/infraestructure/controller/dtos/ErrorDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.inditexecommerce.infraestructure.controller.dtos;
-
-public class ErrorDTO {
-    public String errorMessage;
-}
--- src/main/java/com/example/inditexecommerce/infraestructure/controller/dtos/request/FilteredPriceRequestDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.inditexecommerce.infraestructure.controller.dtos.request;
-
-public class FilteredPriceRequestDTO {
-    public String productId;
-    public String date;
-    public String brandId;
-}

f3407043d414469113ae1dc005ad67fc3576cb8a
brunokiryluk <51805262+brunokiryluk@users.noreply.github.com>
2023-12-08 12:59:41 -0300

    Add Parse Exc and ResponseStatusExc in controller advice

--- src/main/java/com/example/inditexecommerce/infraestructure/controller/exception/ApiErrorHandler.java
+++ src/main/java/com/example/inditexecommerce/infraestructure/controller/exception/ApiErrorHandler.java
@@ -5,8 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 
 @ControllerAdvice
 public class ApiErrorHandler {
@@ -27,5 +29,23 @@ public class ApiErrorHandler {
                 new ErrorResponse(
                         "INTERNAL_SERVER_ERROR", ex.getMessage() != null ? ex.getMessage() : "", ex.toString()),
                 HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ParseException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleParseException(ParseException ex) {
+        return new ResponseEntity<>(
+                new ErrorResponse(
+                        "BAD_REQUEST", ex.getMessage() != null ? ex.getMessage() : "", ex.toString()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleParseException(ResponseStatusException ex) {
+        return new ResponseEntity<>(
+                new ErrorResponse(
+                        "BAD_REQUEST", ex.getMessage() != null ? ex.getMessage() : "", ex.toString()),
+                HttpStatus.BAD_REQUEST);
     }
 }
